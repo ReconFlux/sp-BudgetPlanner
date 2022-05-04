601cf5084637a8c096ec98e46f042ac45ba583cb
Stephen Burtrum <tsgtrecon117@gmail.com>
2022-05-04 12:18:48 -0500

    Added the chart But needs work

--- src/Tabs/Charts.ts
+++ src/Tabs/Charts.ts
@@ -23,13 +23,65 @@ export class ChartsTab {
     private loadCategories() {
         this._categories = [];
         if (DataSource.TransItems) {
+            for (let i = 0; i < DataSource.TransItems.length; i++) {
+                let item = DataSource.TransItems[i];
+                let category = (item.category || "");
 
+                this._categories.push(category);
+            }
         }
     };
 
     // Load Transactions
-    private loadTransactions() { };
+    private loadTransactions() {
+        if (DataSource.TransItems) {
+            for (let i = 0; i < DataSource.TransItems.length; i++) {
+                let item = DataSource.TransItems[i];
+                this._Transactions.push({
+                    id: item.Id,
+                    x: item.category,
+                    y: item.amount,
+                });
+            }
+
+        }
+    };
     // Render
     private render(el: HTMLElement) {
+        let _canvas = document.createElement("canvas");
+        _canvas.id = "myChart";
+        _canvas.width = 100;
+        _canvas.height = 100;
+        el.appendChild(_canvas);
+
+        const ctx = _canvas.getContext('2d');
+
+        const myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                // Check this
+                labels: this._categories,
+                datasets: [
+                    {
+                        label: "Car",
+                        data: this._Transactions,
+                        backgroundColor: "#000080"
+                    }
+                ]
+            },
+            options: {
+
+                parsing: false,
+                devicePixelRatio: 1,
+                aspectRatio: 3,
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                },
+                animation: false,
+            }
+        });
+
     }
 }
--- src/strings.ts
+++ src/strings.ts
@@ -23,6 +23,6 @@ const Strings = {
     ProjectDescription: "A Budget Planner that is developed for SharePoint 2013/2016/Online.",
     SolutionUrl: ContextInfo.webServerRelativeUrl + "/SiteAssets/sp-BudgetPlanner/index.html",
     SourceUrl: ContextInfo.webServerRelativeUrl,
-    Version: "1.0"
+    Version: "1.1"
 };
 export default Strings;
